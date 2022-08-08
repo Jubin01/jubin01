b2f84b76b4fde681046578d9011ab89dfee6c0db
jubin thomas <athom130@adient.com>
2022-08-08 15:01:57 +0530

    file rename

---
+++ src/app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/tictactoe">tic-tac-toe</router-link>
+  </div>
+  <router-view />
+</template>
+<script>
+export default {
+  setup() {
+    document.write("<style type='text/css'>.noscript { display: none; }</style>");
+  },
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;  
+}
+
+body{
+  background-color: black;
+}
+.noscript{
+  color: lightgray;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
