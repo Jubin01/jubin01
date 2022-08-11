57db7710fa5b8b1d4fc7b5bd9e5f765c5a5ed190
jubin thomas <athom130@adient.com>
2022-08-11 12:11:55 +0530

    added sound effect

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,11 +8,14 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
  import HelloWorld from "@/components/hello-world/hello-world.vue"; // @ is an alias to /src
+ //import sound from '/public/sound.mp3';
+ //const sound = await import("../../public/img/icons/");
+ //console.log(sound);
 
 @Options({
   components: {
     HelloWorld
-  },
+  }
 })
  export default class Home extends Vue {}
 </script>
